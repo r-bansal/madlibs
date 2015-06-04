693303e2a1537472d6d361492ddee398222ea5b1
Linda Kung <lindakung417@gmail.com>
2015-06-04 16:56:53 -0400

    React components now working

--- public/app.js
+++ public/app.js
@@ -4,28 +4,34 @@ var MadLibs = React.createClass({
 		return {
 			adj: '',
 			noun: '',
-			showMadLibsStory: false
+			showMadLibsStory: false,
 		}
 	},
 	handleSubmit: function(event) {
 		event.preventDefault();
 		this.setState({
 			adj: React.findDOMNode(this.refs.adj).value,
-			showMadLibsStory: true
+			showMadLibsStory: true,
 		})
 		console.log('hello', this.state.adj)
+	},
+	resetMadLibs: function() {
+		this.setState({
+			adj: '',
+			noun: '',
+			showMadLibsStory: false
+		})
 	},
 	render: function() {
 		return (
 			<div className="madLibs">
-			<form className="storyForm" onSubmit={this.handleSubmit}>
+				{ this.state.showMadLibsStory ? <div><MadLibsStory adj={this.state.adj} noun={this.state.noun} /><button type="submit" onClick={this.resetMadLibs}>Play Again!</button></div> : <form className="storyForm" ref="StoryForm" onSubmit={this.handleSubmit}>
 				<label>Adjective</label>
 				<input type="text" placeholder="Adjective" ref="adj" valueLink={this.linkState('adj')} />
 				<label>Noun</label>
 				<input type="text" placeholder="Noun" ref="noun" valueLink={this.linkState('noun')} />
 				<input type="Submit" value="Submit" />
-				{ this.state.showMadLibsStory ? <MadLibsStory adj={this.state.adj} noun={this.state.noun} /> : null }
-			</form>
+			</form> }
 			</div>
 		)
 	}
